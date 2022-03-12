278f46f3f80ee91fe8efa09da8a911b5ed35a954
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-12 01:37:11 -0300

    Solve exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let splitStr = string.split("");
+  let reverseAry = splitStr.reverse();
+  let aryToStr = reverseAry.join("");
+  return aryToStr;
 };
 
 // Do not edit below this line
