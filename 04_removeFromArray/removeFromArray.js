45126b43db4dad7754d21c89d5acee6464a0a528
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-12 03:02:24 -0300

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  let argsLen = args.length;
+  let argsArray = args;
+  let itemIdx = -1;
+  for (let i = 0; i < argsLen; i++) {
+    itemIdx = array.indexOf(argsArray[i]);
+    if (itemIdx > -1) {
+      array.splice(itemIdx,1)
+    } else {
+      continue;
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
