6261bc00b3a45b8b593248fad75f9c1fd1c84568
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-23 19:49:16 -0300

    Solve exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	const addition = a + b;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  const subtraction = a - b;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let result = 0;
+  if (a.length > 0) {
+    for ( i of a) {
+      result += +i;
+    }
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1;
+  if (a.length > 0) {
+    for (i of a) {
+      result *= +i;
+    }
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  const result = a ** b;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  if (a === 0) {
+    return result;
+  } else {
+    for (let i = 2; i<=a; i++) {
+      result *= i;
+    }
+    return result
+  }
+
 };
 
 // Do not edit below this line
