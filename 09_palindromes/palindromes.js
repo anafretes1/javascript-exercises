1d34e1aa24b753f31f3161bdd35d5726b422c39f
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-24 09:31:12 -0300

    Solve exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let regExp = /[\W]/g; // make a search to exclude non-alphanumeric characters
+  let downStr = str.toLowerCase();
+  downStr = downStr.replace(regExp, ''); // here we eliminate all unwanted characters
+  let reverseStrArr = downStr.split('').reverse();
+  let reverseProper = reverseStrArr.join('');
+  return reverseProper === downStr;
 
 };
 
