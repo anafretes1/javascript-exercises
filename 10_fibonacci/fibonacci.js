51cd05fc56154c44cf21d6889a03d99ed603b031
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-24 20:21:29 -0300

    Solve exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+  Number(a);
+  let arr = [0,1];
 
+  if (a > -1){
+    for (let i = 0; i < a ; i++) {
+      arr.push(arr[i] + arr[i+1]);
+    }
+    return arr[a];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
