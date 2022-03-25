a8f22a31328f3c05712fa887e95bd79e6ae7f4dd
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-25 15:21:31 -0300

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+  const pLen = people.length-1;
+
+  for (let i = 0; i < pLen; i++) {
+    if (!people[i].yearOfDeath ) {
+      people[i].yearOfDeath = (new Date()).getFullYear();
+    }
+  }
+
+  const oldest = people.sort(function(a, b) {
+    const firstAge = a.yearOfDeath - a.yearOfBirth;
+    const nextAge = b.yearOfDeath - b.yearOfBirth;
+
+
+    if (firstAge > nextAge) {
+      return 1;
+    } else {
+      return -1;
+    }
+
+
+  });
+
+
+
+  return oldest[pLen];
+
 
 };
 
